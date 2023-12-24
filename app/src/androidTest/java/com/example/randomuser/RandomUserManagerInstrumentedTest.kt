e9401b0fe7772374efeba1b8cd513ea2cc17933c
adriana634 <50183872+adriana634@users.noreply.github.com>
2023-12-24 13:29:01 +0100

    Random user manager implementation and get random users use case

---
+++ app/src/androidTest/java/com/example/randomuser/RandomUserManagerInstrumentedTest.kt
@@ -0,0 +1,56 @@
+package com.example.randomuser
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.randomuser.manager.RandomUserManager
+import com.example.randomuser.service.RandomUserResponse
+import com.example.randomuser.service.RandomUserService
+import com.example.randomuser.service.RetrofitClient
+import junit.framework.TestCase.assertNotNull
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import retrofit2.Response
+import java.io.IOException
+
+class FakeErrorRandomUserService : RandomUserService {
+    override suspend fun getRandomUsers(): Response<RandomUserResponse> {
+        throw IOException("Simulated network error")
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class RandomUserManagerInstrumentedTest {
+
+    private val randomUserManager = RandomUserManager(RetrofitClient.randomUserService)
+    private val fakeErrorRandomUserManager = RandomUserManager(FakeErrorRandomUserService())
+
+    @Test
+    fun getRandomUsers_networkSuccess_returnsMappedUsers(): Unit = runBlocking {
+        // Act
+        val result = randomUserManager.getRandomUsers()
+
+        // Assert
+        assertEquals(true, result.isSuccessful)
+
+        val mappedUsers = result.body()
+        assertNotNull("Mapped users should not be null", mappedUsers)
+        assertNotEquals("Mapped users should not be empty", 0, mappedUsers?.size)
+
+        mappedUsers?.forEach { user ->
+            assertNotNull("User name should not be null", user.name)
+            assertNotNull("User email should not be null", user.email)
+            assertNotNull("User picture should not be null", user.picture)
+        }
+    }
+
+    @Test
+    fun getRandomUsers_networkError_returnsErrorResponse() = runBlocking {
+        // Act
+        val result = fakeErrorRandomUserManager.getRandomUsers()
+
+        // Assert
+        assertEquals(false, result.isSuccessful)
+    }
+}
---
+++ app/src/main/java/com/example/randomuser/manager/RandomUserManager.kt
@@ -0,0 +1,88 @@
+package com.example.randomuser.manager
+
+import android.util.Log
+import com.example.randomuser.features.userList.model.User
+import com.example.randomuser.service.RandomUser
+import com.example.randomuser.service.RandomUserResponse
+import com.example.randomuser.service.RandomUserService
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import retrofit2.Response
+import java.io.IOException
+
+/**
+ * Manager class responsible for handling network operations related to random users.
+ *
+ * @property randomUserService The service interface for making network requests related to random users.
+ */
+class RandomUserManager(private val randomUserService: RandomUserService) {
+
+    /**
+     * TAG for logging purposes.
+     */
+    companion object {
+        private val TAG = RandomUserManager::class.java.simpleName
+    }
+
+    /**
+     * Fetches random users from the network.
+     *
+     * @return Response containing the list of users or an error response.
+     */
+    suspend fun getRandomUsers(): Response<List<User>> {
+        return try {
+            val response = randomUserService.getRandomUsers()
+            handleResponse(response)
+        } catch (e: IOException) {
+            Log.e(TAG, "Network error", e)
+            Response.error(500, createErrorResponse("Network error"))
+        } catch (e: Exception) {
+            Log.e(TAG, "Unexpected error", e)
+            Response.error(500, createErrorResponse("Unexpected error"))
+        }
+    }
+
+    /**
+     * Handles the network response and maps it to a custom result.
+     *
+     * @param response The network response to be handled.
+     * @return Response containing the list of users or an error response.
+     */
+    private fun handleResponse(response: Response<RandomUserResponse>): Response<List<User>> {
+        return if (response.isSuccessful) {
+            val users = response.body()?.results?.map { randomUser ->
+                mapToUser(randomUser)
+            } ?: emptyList()
+
+            Response.success(users)
+        } else {
+            Log.e(TAG, "API error: ${response.code()} ${response.message()}")
+            Response.error(response.code(), createErrorResponse("API error"))
+        }
+    }
+
+    /**
+     * Maps a RandomUser to a User.
+     *
+     * @param randomUser The RandomUser to be mapped.
+     * @return The mapped User.
+     */
+    private fun mapToUser(randomUser: RandomUser): User {
+        return User(
+            "${randomUser.name.title} ${randomUser.name.first} ${randomUser.name.last}",
+            randomUser.email,
+            randomUser.picture.thumbnail
+        )
+    }
+
+    /**
+     * Creates an error response body with the given error message.
+     *
+     * @param errorMessage The error message.
+     * @return The error response body.
+     */
+    private fun createErrorResponse(errorMessage: String): ResponseBody {
+        return "$errorMessage".toResponseBody("application/json".toMediaTypeOrNull())
+    }
+}
